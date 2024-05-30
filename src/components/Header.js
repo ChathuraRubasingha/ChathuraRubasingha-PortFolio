0654bb2b36a0e5714cb5204df0439971fcc0a4dd
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2024-05-30 14:01:11 +0530

    hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../style/Header.css";
 import { DarkModeContext } from "../App";
 import { Dark, Light } from "../theme";
 import Sun from "../assets/Icons/sun-solid.svg";
 import Moon from "../assets/Icons/moon-solid.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faCross, faHamburger, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const Theme = useContext(DarkModeContext);
-  const toggledark = () => {
+  const toggleDark = () => {
     Theme.setIsDark((prevState) => !prevState);
   };
+  const toggleMenu = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <div
       className="header-section"
-      style={Theme.isdark ? { color: Dark.Color,backgroundColor:Dark.FirstBackground } : { color: Light.Color, backgroundColor:Light.FirstBackground }}
+      style={
+        Theme.isdark
+          ? { color: Dark.Color, backgroundColor: Dark.FirstBackground }
+          : { color: Light.Color, backgroundColor: Light.FirstBackground }
+      }
     >
       <div className="left-side">
         <span style={{ color: Dark.Foreground }}>C</span>R
       </div>
       <div className="right-side">
-        <p>Home</p>
-        <p>About</p>
-        <p>Services</p>
-        <p>Skills</p>
-        <p>Portfolio</p>
-        <p>Contact</p>
-        <div onClick={() => toggledark()}>
-          {Theme.isdark ? <img src={Sun} /> : <img src={Moon}/>}
+        <div className={`menu-item ${isOpen ? "open" : ""}`}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Services</p>
+          <p>Skills</p>
+          <p>Portfolio</p>
+          <p>Contact</p>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+         {isOpen?<FontAwesomeIcon icon={faXmark} />:<FontAwesomeIcon icon={faBars} />} 
+        </div>
+        <div onClick={toggleDark}>
+          {Theme.isdark ? <img src={Sun} alt="Light Mode" /> : <img src={Moon} alt="Dark Mode" />}
         </div>
       </div>
     </div>
